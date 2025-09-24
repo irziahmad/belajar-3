ae1fd58e6beda7d1aacf72e86cac200f91005f61
Creasher <irzifahrozi09@gmail.com>
2025-09-24 13:12:08 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/profil', function () {
+    return view('profil');
+});
